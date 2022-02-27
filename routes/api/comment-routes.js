b4edf6e35709af11ee94e359448496f9b98e7570
mohamed0228 <mohamed0228@outlook.fr>
2022-02-27 11:14:30 -0500

    Integrate Reply Functionality with the Front End

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+  } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
@@ -7,9 +12,15 @@ router.route('/:pizzaId').post(addComment);
 // /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
 
+///////////////////
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment)
+  
+///////////////////////
 
-
-
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 
 
